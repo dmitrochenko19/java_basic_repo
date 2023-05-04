f01429c7d1e37517b934fb48f348c58ddf18d3b5
Rabbit <dmitrochencko>
2023-05-04 13:24:29 +0300

    hw7

---
+++ hw7_exception/src/exceptions/BadConnectionException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class BadConnectionException extends Exception {
+    public BadConnectionException(String message){
+        super(message);
+    }
+}
---
+++ hw7_exception/src/exceptions/ItemNotFoundException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+import java.net.SocketTimeoutException;
+
+public class ItemNotFoundException extends Exception {
+    public ItemNotFoundException(String message){
+        super(message);
+    }
+}
